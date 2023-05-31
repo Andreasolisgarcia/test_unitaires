69453a3dc39a14149eeed3cd4adbbd8301acff89
Rea <andrea.solisg@gmail.com>
2023-05-31 23:37:07 +0200

    Sulfuras test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -52,5 +52,38 @@ describe("Gilded Rose", function() {
     expect(items[3].quality).toBe(42);
     expect(items[4].quality).toBe(42);
   });
-
+  // Tester si la qualité de Sulfuras ne se modifie pas
+  // "Sulfuras", étant un objet légendaire, n'a pas de date de péremption et ne perd jamais en qualité (quality)
+  it("Should add 3 to Backstage passes quality when is between (1 to 5)", function() {
+    let items = [ 
+      new Item("Backstage passes to a TAFKAL80ETC concert", 1, 39),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 2, 39),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 3, 39),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 39),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
+    ];
+    const gildedRose = new Shop(items);
+    gildedRose.updateQuality();
+  
+    // const backstagePasses = gildedRose.items.find(item => item.name === "Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].quality).toBe(42);
+    expect(items[1].quality).toBe(42);
+    expect(items[2].quality).toBe(42);
+    expect(items[3].quality).toBe(42);
+    expect(items[4].quality).toBe(42);
+  });
+    // Tester si la qualité de Sulfuras ne se modifie pas
+  // "Sulfuras", étant un objet légendaire, n'a pas de date de péremption et ne perd jamais en qualité (quality)
+  it("Unchanged Sulfuras Quality", function() {
+    let items = [ 
+      new Item("Sulfuras, Hand of Ragnaros", 5, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    ];
+    const gildedRose = new Shop(items);
+    gildedRose.updateQuality();
+  
+    expect(items[0].quality).toBe(80);
+    expect(items[1].quality).toBe(80);
+  });
+  
 });
